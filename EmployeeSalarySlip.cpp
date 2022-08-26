e4d032fd08b3754a066745d33507ad4be8a68fa9
safdar246 <108629825+safdar246@users.noreply.github.com>
2022-08-26 04:38:47 -0400

    Add files via upload

--- EmployeeSalarySlip.cpp
+++ EmployeeSalarySlip.cpp
@@ -17,7 +17,8 @@ public:
 		//float FA = baseSalary * fuelAll/100;
 		// float MA = baseSalary * MedicalAll/100;
 		//GrossSalary = baseSalary + FA + MA;
-		GrossSalary = ((baseSalary * fuelAll) / 100) + ((baseSalary * MedicalAll) / 100) + baseSalary;
+		GrossSalary = baseSalary + fuelAll + MedicalAll;
+		//GrossSalary = ((baseSalary * fuelAll)/100) + ((baseSalary * MedicalAll)/100) + baseSalary;
 		cout << "GrossSalary of Manager is : " << GrossSalary << endl;
 	}
 };
@@ -26,7 +27,8 @@ public:
 	int GrossSalary;
 	void PrintSalarySlip(float baseSalary, float fuelAll, float MedicalAll){
 		EmployeeSalarySlip::PrintSalarySlip();
-		GrossSalary = ((baseSalary * fuelAll) / 100) + ((baseSalary * MedicalAll) / 100) + baseSalary;
+		GrossSalary = baseSalary + fuelAll + MedicalAll;
+		// GrossSalary = ((baseSalary * fuelAll)/100) + ((baseSalary * MedicalAll)/100) + baseSalary;
 		cout << "GrossSalary of Engineer is : " << GrossSalary << endl;
 	}
 };
@@ -35,7 +37,8 @@ public:
 	int GrossSalary;
 	void PrintSalarySlip(float baseSalary, float fuelAll, float MedicalAll){
 		EmployeeSalarySlip::PrintSalarySlip();
-		GrossSalary = ((baseSalary * fuelAll) / 100) + ((baseSalary * MedicalAll) / 100) + baseSalary;
+		GrossSalary = baseSalary + fuelAll + MedicalAll;
+		// GrossSalary = ((baseSalary * fuelAll)/100) + ((baseSalary * MedicalAll)/100) + baseSalary;
 		cout << "GrossSalary of Analyst is : " << GrossSalary << endl;
 	}
 };
